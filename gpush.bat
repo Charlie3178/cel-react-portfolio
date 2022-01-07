cbb64477fd4a846d3555db5350a050990d9c37c2
Charlie3178 <charlielovejoy@bellsouth.net>
2022-01-07 16:06:32 -0600

    Initial Modal behavior Added

---
+++ gpush.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Initial Modal behavior Added"
+git push origin main
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import BlogModal from "../modals/blog-modal";
 
 class Blog extends Component {
   constructor() {
@@ -12,23 +13,36 @@ class Blog extends Component {
       blogItems: [],
       totalCount: 0,
       currentPage: 0,
-      isLoading: true
+      isLoading: true,
+      blogModalIsOpen: false,
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
-    this.activateInfiniteScroll();
+    this.onScroll = this.onScroll.bind(this);
+    window.addEventListener("scroll", this.onScroll, false);
+    this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
   }
 
-  activateInfiniteScroll() {
-    window.onscroll = () => {
-      if (
-        window.innerHeight + document.documentElement.scrollTop ===
-        document.documentElement.offsetHeight
-      ) {
-        this.getBlogItems();
-        console.log("get more posts");
-      }
-    };
+  handleNewBlogClick() {
+    this.setState({
+      blogModalIsOpen: true
+    });
+  }
+
+  onScroll() {
+    if (
+      this.state.isLoading ||
+      this.state.blogItems.length === this.state.totalCount
+    ) {
+      return;
+    }
+
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      this.getBlogItems();
+    }
   }
 
   getBlogItems() {
@@ -37,12 +51,17 @@ class Blog extends Component {
     });
 
     axios
-      .get("https://clovejoy3178.devcamp.space/portfolio/portfolio_blogs", {
-        withCredentials: true
-      })
+      .get(
+        `https://clovejoy3178.devcamp.space/portfolio/portfolio_blogs?page=${this
+          .state.currentPage}`,
+        {
+          withCredentials: true
+        }
+      )
       .then(response => {
+        console.log("gettting", response.data);
         this.setState({
-          blogItems: response.data.portfolio_blogs,
+          blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
           totalCount: response.data.meta.total_records,
           isLoading: false
         });
@@ -56,6 +75,10 @@ class Blog extends Component {
     this.getBlogItems();
   }
 
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll, false);
+  }
+
   render() {
     const blogRecords = this.state.blogItems.map(blogItem => {
       return <BlogItem key={blogItem.id} blogItem={blogItem} />;
@@ -63,13 +86,14 @@ class Blog extends Component {
 
     return (
       <div className="blog-container">
+        <BlogModal modelIsOpen={this.state.blogModalIsOpen} />
         <div className="content-container">{blogRecords}</div>
 
-        {this.state.isLoading ?(
-        <div className="spinner-wrapper">
-          <FontAwesomeIcon icon="spinner" spin />
-        </div>
-        ): null}
+        {this.state.isLoading ? (
+          <div className="content-loader">
+            <FontAwesomeIcon icon="spinner" spin />
+          </div>
+        ) : null}
       </div>
     );
   }
